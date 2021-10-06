df9729db1a5e8c75b7b88743c2f0c8f6ff6dc700
ritik ranjan <rritik772@outlook.com>
2021-10-06 23:38:45 +0530

    Project Initialized

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Login from './Components/Login/Login';
+
+function App() {
+  return (
+    <>
+      <h1>Hello, World</h1>
+      <Login/>
+    </>
+  )
+}
+
+export default App
