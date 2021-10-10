6a50c780f15c43d1c888e9e015d7aa3bbca56408
ritik ranjan <rritik772@outlook.com>
2021-10-10 08:38:27 +0530

    Qr code generator added

--- src/Components/Index/Index.tsx
+++ src/Components/Index/Index.tsx
@@ -1,16 +1,19 @@
 import Dashboard from "../Dashboard/Dashboard";
+import GenerateQrCode from "../GenerateQrCode/GenerateQrCode";
+import Login from "../Login/Login";
+import SignUp from "../SignUp/SignUp";
 import "./Index.css"
 
 function Index() {
   return (
     <>
       <nav className="top-0 m-5 p-5 bg-sky-500 rounded-lg text-center sticky">
-        <span className="text-2xl text-bold text-white tracking-widest font-Sharp-sans align-middle">MARK ME!</span>
+        <span className="text-2xl text-bold text-white tracking-widest font-sharp-sans align-middle">MARK ME!</span>
       </nav>
       <main className="">
-        <Dashboard/>
+        <SignUp/>
       </main>
-      <footer className="w-full space-x-12 text-center absolute bottom-4">
+      <footer className="w-full my-5 space-x-12 text-center lg:absolute bottom-4">
         <a href="Contact">Contact Me</a>
         <a href="About_me">About Me</a>
       </footer>
