36108d98cf35009ed14e31e1fc9d463d632a05ca
ritik ranjan <rritik772@outlook.com>
2021-10-21 20:10:14 +0530

    review completed

--- src/Components/ScanQrCode/ScanQrCode.tsx
+++ src/Components/ScanQrCode/ScanQrCode.tsx
@@ -41,15 +41,15 @@ export function ScanQrCode() {
         <span className="block my-5 w-full text-3xl text-center font-plex-sans">Mark Attendence</span>
       </div>
       {(alert) && <MessageBox message={alert}/>}
-      <main className="md:w-1/2 lg:w-5/12 xl:w-4/12 mx-auto p-5 space-y-5 4 shadow-lg">
+      <main className="md:w-1/2 lg:w-5/12 xl:w-4/12 mx-auto p-5 space-y-5 4 shadow-lg bg-white">
         <div className="flex space-x-4">
           <button className="w-full py-2 rounded-md bg-sky-500 text-white font-plex-sans-medium transition duration-300 hover:bg-blue-500 hover:shadow-lg" onClick={() => {setAlert(null); setToggleCamera()}}>{(toggleCamera)?'Stop':'Start'} Scanning</button>
         </div>
         {(toggleCamera)?
          <section className="flex justify-center rounded-md overflow-hidden p-5 border-4 border-sky-500 bg-white">
            <QrReader
-             delay={50}
-             style={{height: 340}}
+             delay={100}
+             style={{width: '100%'}}
              onError={(e: any) => setAlert(new Message(0, "Something went wrong."))}
              onScan={(e: any) => handleScanningResult(e)}
            />
