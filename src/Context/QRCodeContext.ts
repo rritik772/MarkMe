fa8185066d93dbf70e17d512721e3b3ae7b15532
ritik ranjan <rritik772@outlook.com>
2021-10-31 15:28:38 +0530

    repaired scanning invalid qr code

--- src/Context/QRCodeContext.ts
+++ src/Context/QRCodeContext.ts
@@ -149,8 +149,8 @@ export const destroyQRCode = async ( docRef: string ): Promise<Message> => {
 export const barcodeExist = async ( docRef: string ): Promise<Message> => {
   const document = await getDoc(doc(firestore, `qrcode/${docRef}`).withConverter(QRCodeModalConverter));
 
-  if ( document.data()?.destroyed ){
-    return new Message(0, "Invalid Qr Code");
+  if ( document.data() && document.data().destroyed === false ){
+    return new Message(2, "Valid Qr Code");
   }
-  return new Message(2, "Valid Qr Code");
+  return new Message(0, "Invalid Qr Code");
 }
